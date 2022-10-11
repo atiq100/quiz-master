51e1de0a22ea8e008eff65f806677c2744202247
atiq100 <atiqur1997rj@gmail.com>
2022-10-11 17:13:26 +0600

    rendered all options with checkbox

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,24 +1,36 @@
+import React from "react";
 
-import React from 'react';
+const Questions = ({ quiz }) => {
+  const { question, options, correctAnswer, id } = quiz;
 
-const Questions = ({quiz}) => {
-    const { question, options, correctAnswer, id } = quiz;
-    
-    return (
-        
-             <div className="card w-6/12 bg-base-100 shadow-xl">
+  return (
+    <div className="card w-full md:w-6/12 bg-base-100 shadow-xl">
       <div className="card-body">
-        <h2 className="card-title">{question.slice(3,-4)}</h2>
+      <div className="card-actions justify-end">
+          <button className="btn btn-primary btn-sm">View Answer</button>
+        </div>
+        <h2 className="card-title">{question.slice(3, -4)}</h2>
         <div>
+          <div className="form-control items-start ">
+           {
+            options.map((p)=>
+                <label className="cursor-pointer label">
+              
+                <input
+                  type="checkbox"
+                  checked
+                  className="checkbox checkbox-primary"
+                />
+                <span className="label-text ml-2">{p}</span>
+              </label>)
+           }
+           
+          </div>
+        </div>
        
-        </div>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
       </div>
     </div>
-       
-    );
+  );
 };
 
 export default Questions;
